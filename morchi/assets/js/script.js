fea4646b11ec934335d8ff2f9aa7803ceab3e561
FilippAkbas <45150759+FilippAkbas@users.noreply.github.com>
2021-02-09 17:02:52 +0300

    add new page, compresor image, upgrade responsive

--- morchi/assets/js/script.js
+++ morchi/assets/js/script.js
@@ -1,6 +1,7 @@
 let mobileBtn = document.querySelector('.mobile-btn a');
 let mobileMenu = document.querySelector('.mobile-full-area');
 let mobileMenuCloseBtn = document.querySelector('.menu-close a');
+let langItem = document.querySelector('.lang-item');
 
 mobileBtn.addEventListener('click', ()=> {
     mobileMenu.classList.add('active');
@@ -8,4 +9,8 @@ mobileBtn.addEventListener('click', ()=> {
 
 mobileMenuCloseBtn.addEventListener('click', ()=> {
     mobileMenu.classList.remove('active');
+});
+
+langItem.addEventListener('click', ()=> {
+    mobileMenu.classList.toggle('active');
 });
