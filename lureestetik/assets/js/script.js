e0a5c311732a214dfc4f0b07d636bcef7dc03a10
FilippAkbas <45150759+FilippAkbas@users.noreply.github.com>
2021-02-12 22:25:11 +0300

    upgrade

--- lureestetik/assets/js/script.js
+++ lureestetik/assets/js/script.js
@@ -3,7 +3,7 @@ const mainSlider = new Swiper("#main-slider", {
   loop: true,
   direction: "vertical",
   slidesPerView: 1,
-  margin: '25px',
+  margin: "25px",
 
   // If we need pagination
   pagination: {
@@ -15,15 +15,44 @@ const mainSlider = new Swiper("#main-slider", {
 });
 
 const blogSlider = new Swiper("#blog-slider", {
-    // Optional parameters
-    loop: false,
-    slidesPerView: 2,
-  
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
+  // Optional parameters
+  loop: false,
+  slidesPerView: 2,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: false,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
     },
-  
-    // Navigation arrows
-    navigation: false,
-  });
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+
+let mobileBurger = document.querySelector('.header-mobile__btn');
+let mobileCloseMenu = document.querySelector('.close-mobile__btn a');
+
+let headerMobile = document.querySelector('.header-mobile');
+
+mobileBurger.addEventListener('click', ()=>{ 
+  headerMobile.classList.add('active');
+});
+
+mobileCloseMenu.addEventListener('click', ()=>{ 
+  headerMobile.classList.remove('active');
+});
