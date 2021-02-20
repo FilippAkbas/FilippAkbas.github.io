661c29ae30d6eb29efac0dd1423320856e2d4478
FilippAkbas <45150759+FilippAkbas@users.noreply.github.com>
2021-02-20 23:45:11 +0300

    upgrade

--- morchi/assets/js/script.js
+++ morchi/assets/js/script.js
@@ -4,11 +4,12 @@ let mobileMenuCloseBtn = document.querySelector('.menu-close a');
 let langItem = document.querySelector('.lang-item');
 
 mobileBtn.addEventListener('click', ()=> {
-    mobileMenu.classList.add('active');
-});
-
-mobileMenuCloseBtn.addEventListener('click', ()=> {
-    mobileMenu.classList.remove('active');
+    mobileMenu.classList.toggle('active');
+    if(mobileMenu.classList.contains('active')) {
+        mobileBtn.innerHTML = '<img src="assets/img/mobil-menü-open.png" alt="Open menu">';
+    } else {
+        mobileBtn.innerHTML = '<img src="assets/img/mobil-menü-close.png" alt="Open menu">';
+    }
 });
 
 langItem.addEventListener('click', ()=> {
