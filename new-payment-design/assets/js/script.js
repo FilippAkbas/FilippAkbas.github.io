c736ff1d4ff79bab6ddd7ca0364d39c79457532b
FilippAkbas <45150759+FilippAkbas@users.noreply.github.com>
2021-03-01 11:32:47 +0300

    add new payment design

---
+++ new-payment-design/assets/js/script.js
@@ -0,0 +1,5 @@
+if (window.innerWidth <= 768) {
+    document.querySelector("#tabs1").innerHTML = '<img src="../assets/img/icon/placeholder.svg">';
+    document.querySelector("#tabs2").innerHTML = '<img src="../assets/img/icon/credit-card.svg">';
+    document.querySelector("#tabs3").innerHTML = '<img src="../assets/img/icon/credit-card.svg">';
+}
