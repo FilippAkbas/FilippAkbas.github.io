b7ae441f856d98ff8980b798dda2a676c38406f4
FilippAkbas <45150759+FilippAkbas@users.noreply.github.com>
2021-03-01 14:44:37 +0300

    upgrade payment

--- new-payment-design/assets/js/script.js
+++ new-payment-design/assets/js/script.js
@@ -1,5 +1,5 @@
 if (window.innerWidth <= 768) {
-    document.querySelector("#tabs1").innerHTML = '<img src="assets/img/icon/placeholder.svg">';
-    document.querySelector("#tabs2").innerHTML = '<img src="assets/img/icon/credit-card.svg">';
+    document.querySelector("#tabs1").innerHTML = '<img src="assets/img/icon/shipped.svg">';
+    document.querySelector("#tabs2").innerHTML = '<img src="assets/img/icon/analysis.svg">';
     document.querySelector("#tabs3").innerHTML = '<img src="assets/img/icon/credit-card.svg">';
 }
