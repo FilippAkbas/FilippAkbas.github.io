3df22e1b1afa028c072961ac9dee5d784dd74e7f
FilippAkbas <45150759+FilippAkbas@users.noreply.github.com>
2021-11-24 02:17:01 +0300

    upgrade

--- turizm-hospital-2/assets/js/script.js
+++ turizm-hospital-2/assets/js/script.js
@@ -1,21 +1,22 @@
 let burger = document.querySelector('.mobile-burger');
+let navMenu = document.querySelector('.nav-menu');
 let header = document.querySelector('.header');
 
 burger.addEventListener('click', ()=> {
     burger.classList.toggle('active');
 
     if(burger.classList.contains('active')) {
-      header.classList.add('active');
+      navMenu.classList.add('active');
     } else {
-      header.classList.remove('active')
+      navMenu.classList.remove('active')
     }
 });
 
 window.addEventListener('scroll', ()=> {
   const scroll = document.documentElement.scrollTop;
   if (scroll > 0) {
-    header.classList.add("fixed");
+    header.classList.add("no-fixed");
   } else {
-    header.classList.remove("fixed");
+    header.classList.remove("no-fixed");
   }
 })
