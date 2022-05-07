77ee2fc91b4f7291f454b2045b6601eebde4b30f
FilippAkbas <45150759+FilippAkbas@users.noreply.github.com>
2022-05-07 17:14:45 +0300

    add responsive

--- glivera-test/dist/js/main.js
+++ glivera-test/dist/js/main.js
@@ -1,7 +1,19 @@
 let closeBtn = document.querySelector('.close-btn');
+let menu = document.querySelector('nav.menu');
+let HeaderMenu = document.querySelector('.header-menu ');
+let mobileBurger = document.querySelector('.header-mobile-burger');
+let CloseMobileMenu = document.querySelector('.header-menu__close');
 
 closeBtn.addEventListener('click', (e)=> {
   let topHeader = e.target.parentNode.parentNode;
 
   topHeader.style.display = 'none';
+});
+
+mobileBurger.addEventListener('click', (e)=> {
+  HeaderMenu.classList.add('active');
+});
+
+CloseMobileMenu.addEventListener('click', (e)=> {
+  HeaderMenu.classList.remove('active');
 });
